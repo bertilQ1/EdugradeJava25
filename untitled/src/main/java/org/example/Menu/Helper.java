e924d2fcfc509ce69f4ebd39bb9b2a22bc012039
bertilQ1 <gusen.fred@gmail.com>
2025-10-02 15:02:49 +0200

    tog bort den för att jag inte planerar att använda den

--- untitled/src/main/java/org/example/Menu/Helper.java
+++ untitled/src/main/java/org/example/Menu/Helper.java
@@ -1,16 +1,8 @@
 package org.example.Menu;
 
 public interface Helper {
-    static int randomInt(int min, int max){
+    static int randomInt(int min, int max) {
         return (int) Math.round(Math.random() * (max - min + 1) + min); // max - min + 1 == största värdet minus minsta + 1 för att se till att det är största. (denna googlade jag :))
-    }
-
-    static void sleepForMilliSeconds(int time) {
-        try {
-            Thread.sleep(time);
-        } catch(InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 }
 
